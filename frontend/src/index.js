4ad4ecb198c2ed7f5cd945a2f9b5a175412a7aee
ramabasa <ramabasa090@gmail.com>
2025-04-25 20:14:35 -0400

    react-router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./landing_page/home/HomePage.js";
+import Signup from "./landing_page/signup/Signup.js";
+import AboutPage from "./landing_page/about/AboutPage.js";
+import ProductPage from "./landing_page/products/ProductsPage.js";
+import PricingPage from "./landing_page/pricing/PricingPage.js";
+import SupportPage from "./landing_page/support/SupportPage.js";
+import NotFound from "./landing_page/NotFound.js";
+import Navbar from "./Navbar.js";
+import Footer from "./Footer.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <HomePage />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/product" element={<ProductPage />} />
+      <Route path="/pricing" element={<PricingPage />} />
+      <Route path="/support" element={<SupportPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    <Footer />
+  </BrowserRouter>
 );
